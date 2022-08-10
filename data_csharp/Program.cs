3beca5490f6bbdb2410b6e2fbaf6c40c375eea2c
fenghao <fenghao@dropbox.com>
2022-08-10 16:46:10 -0700

    signatureRequestSend

--- data_csharp/Program.cs
+++ data_csharp/Program.cs
@@ -6,7 +6,6 @@ using Newtonsoft.Json.Linq;
 using Org.HelloSign.Api;
 using Org.HelloSign.Client;
 using Org.HelloSign.Model;
-using JsonConverter = Newtonsoft.Json.JsonConverter;
 using JsonSerializer = System.Text.Json.JsonSerializer;
 
 
@@ -21,20 +20,25 @@ class Requester
     private string? operationId;
     private JObject? parameters;
 
+    private readonly string FILE_UPLOADS_DIR =
+         /*"/file_uploads" */
+        "/Users/fenghao/PhpstormProjects/openapi/hellosign-dotnet-sdk/RunSDK/file_uploads";
+
+
     public Requester(string authType, string authKey, string apiServer, string jsonData, bool devMode)
     {
         this.authType = authType.ToLower();
         this.authKey = authKey;
         this.apiServer = apiServer;
         this.devMode = devMode;
-        readJsonData(jsonData);
+        ReadJsonData(jsonData);
     }
 
-    public void run()
+    public void Run()
     {
         try
         {
-            var apiResponse = callFromOperationId();
+            var apiResponse = CallFromOperationId();
             var output = new Dictionary<string, object>
             {
                 ["body"] = apiResponse.Content,
@@ -55,7 +59,7 @@ class Requester
         }
     }
 
-    private void readJsonData(string base64Json)
+    private void ReadJsonData(string base64Json)
     {
         var json = Encoding.UTF8.GetString(Convert.FromBase64String(base64Json));
         var dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
@@ -64,17 +68,32 @@ class Requester
         dictionary.TryGetValue("files", out var filesObj);
         dictionary.TryGetValue("parameters", out var parametersObj);
 
+        data = dataObj as JObject;
+        files = filesObj as JObject;
         parameters = parametersObj as JObject;
     }
 
-    private IApiResponse callFromOperationId()
+    private IApiResponse CallFromOperationId()
     {
-        var accountApi = new AccountApi(getConfiguration());
+        return SignatureRequestApi();
+    }
+
+    private IApiResponse AccountApi()
+    {
+        var accountApi = new AccountApi(GetConfiguration());
         parameters.TryGetValue("account_id", out var accountId);
         return accountApi.AccountGetWithHttpInfo(accountId?.ToString());
     }
 
-    private Configuration getConfiguration()
+    private IApiResponse SignatureRequestApi()
+    {
+        var api = new SignatureRequestApi(GetConfiguration());
+        var sendRequest = JsonConvert.DeserializeObject<SignatureRequestSendRequest>(data.ToString());
+        sendRequest.File = GetFiles("file");
+        return api.SignatureRequestSendWithHttpInfo(sendRequest);
+    }
+
+    private Configuration GetConfiguration()
     {
         var config = new Configuration();
         if (!String.IsNullOrEmpty(apiServer))
@@ -107,6 +126,13 @@ class Requester
         return config;
     }
 
+    public List<Stream> GetFiles(string name)
+    {
+        var jToken = files[name];
+
+        return jToken.Select(filename => new StreamReader(FILE_UPLOADS_DIR + $"/{filename}").BaseStream).ToList();
+    }
+
     static void Main(string[] args)
     {
         var authType = args[0];
@@ -120,7 +146,7 @@ class Requester
         }
 
         var requester = new Requester(authType, authKey, apiServer, jsonData, devMode);
-        requester.run();
+        requester.Run();
     }
 }
 
